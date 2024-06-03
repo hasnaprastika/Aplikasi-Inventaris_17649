4b9d9f4aaca9ed93049f7da057dfd15985d9858f
unknown <Auliahasnaprastika@gmail.com>
2024-06-03 08:24:29 +0700

    Aulia

---
+++ resources/views/pages/users/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('title', 'Users')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Data User</h1>
+                <div class="section-header-button">
+                    <a href="{{ route('user.create') }}" class="btn btn-primary">Tambah users</a>
+                </div>
+            </div>
+            <div class="section-body">
+
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>All Users</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="float-right">
+                                    <form method="GET" action="{{ route('user.index') }}">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="Search" name="name">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="clearfix mb-3"></div>
+
+                                <div class="table-responsive">
+                                    <table class="table-striped table">
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Username</th>
+                                            <th>Role</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        @foreach ($users as $user)
+                                            <tr>
+                                                <td>
+                                                    {{ $user->nama_petugas }}
+                                                </td>
+                                                <td>
+                                                    {{ $user->username }}
+                                                </td>
+                                                <td>
+                                                    {{ $user->level->nama_level }}
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex justify-content-center">
+                                                        <a href='{{ route('user.edit', $user->id_petugas) }}'
+                                                            class="btn btn-sm btn-info btn-icon">
+                                                            <i class="fas fa-edit"></i>
+                                                            Edit
+                                                        </a>
+
+                                                        <form action="{{ route('user.destroy', $user->id_petugas) }}"
+                                                            method="POST" class="ml-2">
+                                                            <input type="hidden" name="_method" value="DELETE" />
+                                                            <input type="hidden" name="_token"
+                                                                value="{{ csrf_token() }}" />
+                                                            <button class="btn btn-sm btn-danger btn-icon confirm-delete">
+                                                                <i class="fas fa-times"></i> Delete
+                                                            </button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                                <div class="float-right">
+                                    {{-- Pagination User --}}
+                                    {{ $users->withQueryString()->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/features-posts.js') }}"></script>
+@endpush
